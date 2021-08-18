80e8e925915f741c1ced4b1d88ac60f6a39af507
mmoomocow <44288823+mmoomocow@users.noreply.github.com>
2021-08-18 13:46:28 +1200

    create question classes

--- console/Program.cs
+++ console/Program.cs
@@ -2,11 +2,35 @@
 
 namespace console
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	public class MultiChoiceQuestion
+	{
+		string questionText;
+		string[] possibleAnswers; // if multiChoice true
+		int correctIndex; // if multiChoice true
+
+		public MultiChoiceQuestion(string text, string[] answers, int correct)
+		{
+			questionText = text;
+			possibleAnswers = answers;
+			correctIndex = correct;
+		}
+	}
+	public class Question
+	{
+		string questionText;
+		string correctIndex; // if multiChoice true
+
+		public Question(string text, string correct)
+		{
+			questionText = text;
+			correctIndex = correct;
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
 }

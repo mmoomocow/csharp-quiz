63bd955b88319122fae2b6d8c5d41a0b9110d338
mmoomocow <44288823+mmoomocow@users.noreply.github.com>
2021-08-26 11:33:14 +1200

    Create multiple question arrays

--- console/Program.cs
+++ console/Program.cs
@@ -22,11 +22,30 @@ namespace console
 		static void Main(string[] args)
 		{
 			Random rnd = new Random();
-			Question[] questions =
-			{
-				new Question("Question 1", (new string[] {"Answer 0", "Answer 1", "Answer 2", "Answer 3"}), 2, "Some kind of explanation for why number 3 is correct..."),
-				new Question("Question 2", (new string[] {"Answer 0", "Answer 1", "Answer 2", "Answer 3"}), 3, "Some kind of explanation for why number 4 is correct..."),
-				new Question("Question 3", (new string[] {"Answer 0", "Answer 1", "Answer 2", "Answer 3"}), 0, "Some kind of explanation for why number 1 is correct..."),
+			Question[] questionsType1 = {
+				new Question("Question 1 A", (new string[] { "Answer 0", "Answer 1", "Answer 2", "Answer 3" }), 3, "Some kind of explanation for why number 1 is correct..."),
+				new Question("Question 1 B", (new string[] { "Answer 0", "Answer 1", "Answer 2", "Answer 3" }), 2, "Some kind of explanation for why number 1 is correct..."),
+				new Question("Question 1 C", (new string[] { "Answer 0", "Answer 1", "Answer 2", "Answer 3" }), 0, "Some kind of explanation for why number 1 is correct..."),
+				new Question("Question 1 D", (new string[] { "Answer 0", "Answer 1", "Answer 2", "Answer 3" }), 1, "Some kind of explanation for why number 1 is correct..."),
+			};
+			Question[] questionsType2 = {
+				new Question("Question 2 A", (new string[] { "Answer 0", "Answer 1", "Answer 2", "Answer 3" }), 1, "Some kind of explanation for why number 1 is correct..."),
+				new Question("Question 2 B", (new string[] { "Answer 0", "Answer 1", "Answer 2", "Answer 3" }), 2, "Some kind of explanation for why number 1 is correct..."),
+				new Question("Question 2 C", (new string[] { "Answer 0", "Answer 1", "Answer 2", "Answer 3" }), 3, "Some kind of explanation for why number 1 is correct..."),
+				new Question("Question 2 D", (new string[] { "Answer 0", "Answer 1", "Answer 2", "Answer 3" }), 2, "Some kind of explanation for why number 1 is correct..."),
+			};
+			Question[] questionsType3 = {
+				new Question("Question 3 A", (new string[] { "Answer 0", "Answer 1", "Answer 2", "Answer 3" }), 3, "Some kind of explanation for why number 1 is correct..."),
+				new Question("Question 3 B", (new string[] { "Answer 0", "Answer 1", "Answer 2", "Answer 3" }), 3, "Some kind of explanation for why number 1 is correct..."),
+				new Question("Question 3 C", (new string[] { "Answer 0", "Answer 1", "Answer 2", "Answer 3" }), 0, "Some kind of explanation for why number 1 is correct..."),
+				new Question("Question 3 D", (new string[] { "Answer 0", "Answer 1", "Answer 2", "Answer 3" }), 2, "Some kind of explanation for why number 1 is correct..."),
+			};
+
+			Question[] questionsType4 = {
+				new Question("Question 4 A", (new string[] { "Answer 0", "Answer 1", "Answer 2", "Answer 3" }), 0, "Some kind of explanation for why number 1 is correct..."),
+				new Question("Question 4 B", (new string[] { "Answer 0", "Answer 1", "Answer 2", "Answer 3" }), 2, "Some kind of explanation for why number 1 is correct..."),
+				new Question("Question 4 C", (new string[] { "Answer 0", "Answer 1", "Answer 2", "Answer 3" }), 1, "Some kind of explanation for why number 1 is correct..."),
+				new Question("Question 4 D", (new string[] { "Answer 0", "Answer 1", "Answer 2", "Answer 3" }), 3, "Some kind of explanation for why number 1 is correct..."),
 			};
 			Question randomQuestion = questions[rnd.Next(questions.Length)];
 
@@ -49,7 +68,7 @@ namespace console
 			else
 			{
 				Console.WriteLine("Thats correct!!");
-			}
+			// }
 
 			Console.ReadLine();
 		}

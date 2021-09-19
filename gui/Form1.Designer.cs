70eba987c2006112dfdf9c34421bbf0cc7987a1a
mmoomocow <44288823+mmoomocow@users.noreply.github.com>
2021-09-19 16:32:34 +1200

    create answer buttons

--- gui/Form1.Designer.cs
+++ gui/Form1.Designer.cs
@@ -29,13 +29,65 @@ namespace gui
         /// </summary>
         private void InitializeComponent()
         {
+            this.answerBtn4 = new System.Windows.Forms.Button();
+            this.answerBtn3 = new System.Windows.Forms.Button();
+            this.answerBtn2 = new System.Windows.Forms.Button();
+            this.answerBtn1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // answerBtn4
+            // 
+            this.answerBtn4.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.answerBtn4.Location = new System.Drawing.Point(422, 330);
+            this.answerBtn4.Name = "answerBtn4";
+            this.answerBtn4.Size = new System.Drawing.Size(350, 100);
+            this.answerBtn4.TabIndex = 7;
+            this.answerBtn4.Text = "Answer button 4";
+            this.answerBtn4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.answerBtn4.UseVisualStyleBackColor = true;
+            // 
+            // answerBtn3
+            // 
+            this.answerBtn3.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.answerBtn3.Location = new System.Drawing.Point(422, 224);
+            this.answerBtn3.Name = "answerBtn3";
+            this.answerBtn3.Size = new System.Drawing.Size(350, 100);
+            this.answerBtn3.TabIndex = 6;
+            this.answerBtn3.Text = "Answer button 3";
+            this.answerBtn3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.answerBtn3.UseVisualStyleBackColor = true;
+            // 
+            // answerBtn2
+            // 
+            this.answerBtn2.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.answerBtn2.Location = new System.Drawing.Point(422, 118);
+            this.answerBtn2.Name = "answerBtn2";
+            this.answerBtn2.Size = new System.Drawing.Size(350, 100);
+            this.answerBtn2.TabIndex = 5;
+            this.answerBtn2.Text = "Answer button 2";
+            this.answerBtn2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.answerBtn2.UseVisualStyleBackColor = true;
+            // 
+            // answerBtn1
+            // 
+            this.answerBtn1.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.answerBtn1.Location = new System.Drawing.Point(422, 12);
+            this.answerBtn1.Name = "answerBtn1";
+            this.answerBtn1.Size = new System.Drawing.Size(350, 100);
+            this.answerBtn1.TabIndex = 4;
+            this.answerBtn1.Text = "Answer button 1";
+            this.answerBtn1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.answerBtn1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 361);
+            this.ClientSize = new System.Drawing.Size(784, 441);
+            this.Controls.Add(this.answerBtn4);
+            this.Controls.Add(this.answerBtn3);
+            this.Controls.Add(this.answerBtn2);
+            this.Controls.Add(this.answerBtn1);
             this.Name = "Form1";
             this.Text = "Quiz App";
             this.ResumeLayout(false);
@@ -43,6 +95,11 @@ namespace gui
         }
 
         #endregion
+
+        private System.Windows.Forms.Button answerBtn4;
+        private System.Windows.Forms.Button answerBtn3;
+        private System.Windows.Forms.Button answerBtn2;
+        private System.Windows.Forms.Button answerBtn1;
     }
 }
 
